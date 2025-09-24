9a17f55a561032751caadb2b9730bb970ed05957
Mystic#9697 <shadow6060@users.noreply.github.com>
2025-09-24 12:45:34 -0400

    Add files via upload

---
+++ src/prefixCommands/economy/crime.ts
@@ -0,0 +1,38 @@
+import { db } from "../../database/database";
+import { upsertUserInfo } from "../../database/userInfo";
+import { constants, text } from "../../providers/config";
+import { format } from "../../utils/string";
+import pms from "pretty-ms";
+import { randRange, sampleArray } from "../../utils/utils";
+import { getCooldownTimeRemaining, isOnCooldown, setCooldown } from "../../utils/MysticUtils/Commands/cooldownManager";
+import { PrefixCommand } from "../../structures/prefixs/prefixCommand";
+
+export const command = new PrefixCommand("crime", "Try your chances at doing crime!")
+	.setCategory("economy")
+	.setPrefixExecutor(async (message) => {
+		const userId = message.author.id;
+
+		// Check cooldown
+		if (isOnCooldown(userId, "crime")) {
+			const cooldownTime = getCooldownTimeRemaining(userId, "crime"); // Use a function to get remaining time
+			await message.reply(
+				format(text.errors.cooldown, pms(cooldownTime, { compact: true, secondsDecimalDigits: 1 }))
+			);
+			return;
+		}
+
+		const result = Math.random() < 0.5 ? "Failure" : "Success";
+		const info = await upsertUserInfo(message.author);
+		const obtained = randRange(...constants.crime.amountRange);
+
+		// Set cooldown
+		setCooldown(userId, "crime");
+
+		if (result === "Failure") {
+			await db.userInfo.update({ where: { id: info.id }, data: { balance: { decrement: obtained } } });
+			await message.reply(format(sampleArray(text.commands.crime.failure), `\`$${-obtained}\``));
+		} else {
+			await db.userInfo.update({ where: { id: info.id }, data: { balance: { increment: obtained } } });
+			await message.reply(format(sampleArray(text.commands.crime.sucess), `\`$${obtained}\``));
+		}
+	});
---
+++ src/prefixCommands/economy/work.ts
@@ -0,0 +1,38 @@
+/* eslint-disable linebreak-style */
+import { db } from "../../database/database";
+import { upsertUserInfo } from "../../database/userInfo";
+import { constants, text } from "../../providers/config";
+import { PrefixCommand } from "../../structures/prefixs/prefixCommand";
+import { format } from "../../utils/string";
+import pms from "pretty-ms";
+import { getCooldownTimeRemaining, isOnCooldown, setCooldown } from "../../utils/MysticUtils/Commands/cooldownManager";
+import { randRange, sampleArray } from "../../utils/utils";
+
+export const command = new PrefixCommand("work", "Gets you some money.")
+	.setCategory("economy")
+	.setPrefixExecutor(async (message) => {
+		const userId = message.author.id;
+
+		// Cooldown check
+		if (isOnCooldown(userId, "work")) {
+			const cooldownTime = getCooldownTimeRemaining(userId, "work");
+			await message.reply(
+				format(text.errors.cooldown, pms(cooldownTime, { compact: true, secondsDecimalDigits: 1 }))
+			);
+			return;
+		}
+
+		// Ensure user exists
+		const info = await upsertUserInfo(message.author);
+		const obtained = randRange(...constants.work.amountRange);
+
+		// Set cooldown
+		setCooldown(userId, "work");
+
+		await db.userInfo.update({
+			where: { id: info.id },
+			data: { balance: { increment: obtained } },
+		});
+
+		await message.reply(format(sampleArray(text.commands.work.responses), `\`$${obtained}\``));
+	});
