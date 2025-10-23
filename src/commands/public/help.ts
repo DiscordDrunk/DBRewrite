f452f5c960710341058a0d4c50bfbb8024c41a9b
Mystic#9697 <shadow6060@users.noreply.github.com>
2025-10-23 15:00:30 -0400

    Add files via upload

--- src/commands/public/help.ts
+++ src/commands/public/help.ts
@@ -9,6 +9,7 @@ import {
 import { Command } from "../../structures/Command";
 import { slashCommandRegistry } from "../../providers/commandManager";
 import type { ChatInputCommandInteraction } from "discord.js";
+import { safeReply } from "../../components/SafeInteractions";
 
 const groupCommandsByCategory = (commands: Command[]) => {
 	const categories: Record<string, Command[]> = {};
